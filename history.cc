24b84bfb37b6ce4a267102351b454e08037e6219
Daniel M <nobody@localhost>
2025-11-22 10:15:52 -0300

    cleaning up history

--- history.cc
+++ history.cc
@@ -1,11 +1,5 @@
 #include "history.h"
 
-void History::clear() {
-  container_.clear();
-  lines_.clear();
-  lines_.push_back(0);
-}
-
 uint64_t History::pushBack(rune::Rune && rune, uint16_t carriageReturn) {
   bool newLine = false;
   switch (rune.character) {
@@ -34,27 +28,10 @@ uint64_t History::pushBack(rune::Rune && rune, uint16_t carriageReturn) {
   }
 
   if (newLine) {
-#if 0
-    if (0 < cap_ && lines_.size() > cap_) {
-      container_.erase(container_.begin(), container_.begin() + lines_[0]);
-      lines_.erase(lines_.begin());
-    }
-#endif
-    lines_.push_back(container_.size());
+    ++lines_;
   }
 
   return container_.size();
-}
-
-History::Container::const_iterator History::line(const uint64_t line) const {
-  assert(line >= firstLine_);
-  assert(lines_.size() > line - firstLine_);
-  return container_.begin() + lines_[line];
-}
-
-/* does changing a cap invalidate iterators ? */
-void History::setCap(const uint32_t cap) {
-  cap_ = cap;
 }
 
 History::ReverseIterator History::reverseIterator(const uint64_t i) {
@@ -62,16 +39,6 @@ History::ReverseIterator History::reverseIterator(const uint64_t i) {
   ReverseIterator iterator = container_.rbegin();
   iterator += container_.size() - i;
   return iterator;
-}
-
-void History::print() {
-  for (rune::Rune & rune : container_) {
-    if (256 > rune.character) {
-      std::cout << static_cast<char>(rune.character) << std::flush;
-    } else {
-      std::cout << '.' << std::flush;
-    }
-  }
 }
 
 uint64_t History::erase(const uint64_t count) {
--- history.h
+++ history.h
@@ -11,29 +11,23 @@
 
 #include "rune.h"
 
-struct History {
+class History {
   using Container = std::vector<rune::Rune>;
+
+public:
   using Iterator = Container::const_iterator;
   using ReverseIterator = Container::const_reverse_iterator;
 
-  auto clear() -> void;
   auto erase(const uint64_t) -> uint64_t;
-  auto line(const uint64_t) const -> Iterator;
-  auto lines() const -> std::size_t { return lines_.size(); }
+  auto lines() const -> std::size_t { return lines_; }
   auto operator [] (const uint64_t i) const { return container_.operator[](i); }
-  auto print() -> void;
   auto pushBack(rune::Rune &&, uint16_t cr = 0) -> uint64_t;
   auto rbegin() const -> ReverseIterator { return container_.rbegin(); }
   auto rend() const -> ReverseIterator { return container_.rend(); }
   auto reverseIterator(const uint64_t) -> ReverseIterator;
-  auto setCap(const uint32_t) -> void;
   auto size() const -> uint64_t { return container_.size(); }
 
 private:
-  using Lines = std::deque<uint64_t>;
   Container container_;
-  Lines lines_ = { /* first line always starts at position */ 0 };
-  uint64_t cap_ = 0;
-  uint64_t firstLine_ = 0;
-  uint64_t offset_ = 0;
+  uint64_t lines_ = 0;
 };
